0c7b290b6d0d08913270a041244590c7f9f481ab
gujun <gujun@qiyi.com>
2020-01-14 17:50:04 +0800

    add listDemo

---
+++ src/main/groovy/org/example/DemoList.groovy
@@ -0,0 +1,15 @@
+package org.example
+
+/**
+ *
+ * @author gujun@qiyi.com
+ * @since 2020/1/14 5:36 下午
+ */
+class DemoList {
+    static void main(String[] args) {
+        def list = [1,2,3,4,5]
+        println(list)
+        list.add(6)
+        println(list)
+    }
+}
